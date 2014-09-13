bff66f59a11f640c8d0a755e0874ea28d7750284
olivieer <yves.olli@gmail.com>
2014-09-13 13:41:09 -0400

    Yuga: Update build fingerprint (10.5.1.A.0.283)
    Change-Id: I495ed85af50d2b7bf7cb13418503ce9983012c4f

--- du.mk
+++ du.mk
@@ -5,7 +5,7 @@ $(call inherit-product, vendor/du/config/common.mk)
 # Inherit Yuga Product configuration
 $(call inherit-product, device/sony/yuga/full_yuga.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.2.2/10.3.A.0.423/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.2.2 10.3.A.0.423 WP5_rg test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.4.4 10.5.1.A.0.283 5P53rQ release-keys"
 
 PRODUCT_NAME := du_yuga
 PRODUCT_DEVICE := yuga
