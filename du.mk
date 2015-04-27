e98ea538c62394944743d00233cfb2b9d9f2732a
Humberto Borba <humberos@gmail.com>
2015-04-27 17:45:24 -0300

    yuga: increase bootanimation size to 960x640
    the current bootanimation size is 640x480 ≃ 4,3MB
    
    new bootanimation size is 960x640 ≃ 3,4MB
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ia55b2253277059afdc90dcefb0d039d0e7a8f5cf

--- du.mk
+++ du.mk
@@ -1,6 +1,9 @@
 # Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/du/config/gsm.mk)
 
+# Bootanimation
+TARGET_BOOTANIMATION_SIZE := 960x640
+
 # Inherit Omni product configuration
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
 
